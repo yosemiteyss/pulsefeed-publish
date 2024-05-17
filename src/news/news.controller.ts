d3a3d8339dd8f4472819181b6d8960a0c10f4207
yosemiteyss <kevinhonasdf@gmail.com>
2024-05-17 17:15:01 +0800

    update

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,8 +1,6 @@
 import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 import { PublishService } from './publish.service';
 import { Controller } from '@nestjs/common';
-import { FeedEntity } from '@common/db';
-import { DeepPartial } from 'typeorm';
 
 @Controller()
 export class NewsController {
@@ -10,15 +8,6 @@ export class NewsController {
 
   @MessagePattern('publish-feed')
   async publishFeed(@Payload() data: any, @Ctx() context: RmqContext) {
-    const channel = context.getChannelRef();
-    const originalMessage = context.getMessage();
-
-    try {
-      const feed: DeepPartial<FeedEntity> = JSON.parse(data);
-      await this.publishNewsService.publishFeed(feed);
-      channel.ack(originalMessage); // Acknowledge message after processing
-    } catch (err) {
-      channel.nack(originalMessage);
-    }
+    await this.publishNewsService.publishFeed(data, context);
   }
 }
--- src/news/publish.service.ts
+++ src/news/publish.service.ts
@@ -1,3 +1,4 @@
+import { RmqContext } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, Repository } from 'typeorm';
 import { LoggerService } from '@common/logger';
@@ -11,16 +12,24 @@ export class PublishService {
     private readonly logger: LoggerService,
   ) {}
 
-  async publishFeed(feed: DeepPartial<FeedEntity>) {
+  async publishFeed(data: any, context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    let feed: DeepPartial<FeedEntity>;
+
     try {
+      feed = JSON.parse(data);
+
       const created = this.feedsRepository.create(feed);
       const inserted = await this.feedsRepository.save(created);
-
       const itemsCount = inserted.items?.length ?? 0;
 
       this.logger.log(PublishService.name, `Published news: ${itemsCount}, feed ${inserted.link}`);
+      channel.ack(originalMessage); // Acknowledge message after processing
     } catch (err) {
       this.logger.error(PublishService.name, `Failed to publish news, feed: ${feed.link}`, err);
+      channel.nack(originalMessage);
     }
   }
 }
