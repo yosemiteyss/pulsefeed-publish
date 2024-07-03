92d2379bda47fc040eda5d3aa4d9ece300c9014a
yosemiteyss <kevinhonasdf@gmail.com>
2024-07-03 14:26:49 +0800

    update

--- src/publish-feed/repository/feed.repository.ts
+++ src/publish-feed/repository/feed.repository.ts
@@ -1,10 +1,4 @@
-import {
-  ArticleEntity,
-  FeedEntity,
-  LanguageEntity,
-  TBL_FEEDS_ARTICLES,
-  TBL_LANGUAGES_ARTICLES,
-} from '@common/db';
+import { ArticleEntity, FeedEntity, TBL_FEEDS_ARTICLES } from '@common/db';
 import { DataSource, EntityManager, In } from 'typeorm';
 import { Article, Feed } from '@common/model';
 import { Injectable } from '@nestjs/common';
@@ -15,30 +9,15 @@ export class FeedRepository {
 
   async create(feed: Feed): Promise<ArticleEntity[]> {
     return await this.dataSource.transaction(async (entityManager) => {
-      await this.insertFeed(entityManager, feed);
-
       const articles = await this.insertArticles(entityManager, feed.items);
       await this.insertFeedArticleRelation(entityManager, feed.id, articles);
 
-      await this.insertArticleLangRelation(entityManager, articles);
+      await this.insertFeed(entityManager, feed);
 
       return articles;
     });
   }
 
-  private async insertFeed(entityManager: EntityManager, feed: Feed) {
-    await entityManager
-      .createQueryBuilder()
-      .insert()
-      .into(FeedEntity)
-      .values(feed)
-      .orUpdate(['title', 'description'], ['id'], { skipUpdateIfNoValuesChanged: true })
-      .execute();
-  }
-
-  /**
-   * Returns inserted article ids.
-   */
   private async insertArticles(
     entityManager: EntityManager,
     articles: Article[],
@@ -64,6 +43,16 @@ export class FeedRepository {
     });
   }
 
+  private async insertFeed(entityManager: EntityManager, feed: Feed) {
+    await entityManager
+      .createQueryBuilder()
+      .insert()
+      .into(FeedEntity)
+      .values(feed)
+      .orUpdate(['title', 'description'], ['id'], { skipUpdateIfNoValuesChanged: true })
+      .execute();
+  }
+
   private async insertFeedArticleRelation(
     entityManager: EntityManager,
     feedId: string,
@@ -78,26 +67,6 @@ export class FeedRepository {
       .createQueryBuilder()
       .insert()
       .into(TBL_FEEDS_ARTICLES)
-      .values(relations)
-      .orIgnore()
-      .execute();
-  }
-
-  private async insertArticleLangRelation(entityManager: EntityManager, articles: ArticleEntity[]) {
-    const relations = [];
-    for (const article of articles) {
-      for (const language of article.languages as LanguageEntity[]) {
-        relations.push({
-          languageKey: language.key,
-          articleId: article.id,
-        });
-      }
-    }
-
-    await entityManager
-      .createQueryBuilder()
-      .insert()
-      .into(TBL_LANGUAGES_ARTICLES)
       .values(relations)
       .orIgnore()
       .execute();
